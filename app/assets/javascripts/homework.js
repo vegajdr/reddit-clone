08e183d8e3fae7d70884e069b4688aa02d27b42e
vegajdr <jorgevp5@gmail.com>
2016-06-27 16:37:58 -0400

    Completed Level 1

--- app/assets/javascripts/homework.js
+++ app/assets/javascripts/homework.js
@@ -1,7 +1,71 @@
 
 
-
-
 $(document).ready(function(){
-  
-})
+
+  var total = 0;
+  var result = $(".result");
+
+  resultColor = function(score, result){
+    if (total < 10) {
+      result.removeClass("red");
+    }
+    else {
+      result.addClass("red");
+    }
+  };
+
+  $("#upvote").click(function(){
+    console.log("Anything");
+    total = total + 1;
+    result.text(total);
+
+    resultColor(total, result);
+
+    // if (total > 9) {
+    //   result.addClass("red")
+    // }
+  });
+
+  $("#downvote").click(function(){
+    total = total - 1;
+    result.text(total);
+
+    if (total < 10) {
+      result.removeClass("red");
+    }
+  });
+
+  $("#reset").click(function(){
+    now = new Date();
+    var date = now.toLocaleDateString();
+    var time = now.toLocaleTimeString();
+
+    var savedTotal = total;
+
+
+    result.removeClass("red");
+
+    if (total !== 0){
+      console.log(savedTotal);
+      total = 0;
+      result.text(total);
+
+      var logEntry = $("<li>");
+      logEntry.append("Reset from ");
+      logEntry.append($("<span>").text(savedTotal));
+      logEntry.append(" has been saved on: " + date + " at " +  time);
+
+      $(".log").append(logEntry);
+    }
+    else {
+      alert("You can't reset 0");
+    }
+  });
+
+  $("#clear-log").click(function(){
+    $(".log").empty();
+  });
+
+
+
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
 class UsersController < ApplicationController
   skip_after_action :verify_authorized, only: [:destroy]
-
 end
