c81239bcf96168857c02c9261a4d179d022c2ab8
vegajdr <jorgevp5@gmail.com>
2016-06-28 16:49:26 -0400

    Fixed migration

--- db/migrate/20160623201241_create_moderators.rb
+++ db/migrate/20160623201241_create_moderators.rb
@@ -1,8 +1,8 @@
 class CreateModerators < ActiveRecord::Migration
   def change
     create_table :moderators do |t|
-      t.belongs_to :user_id, index: true, foreign_key: true
-      t.belongs_to :room_id, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :room, index: true, foreign_key: true
 
       t.timestamps null: false
     end
